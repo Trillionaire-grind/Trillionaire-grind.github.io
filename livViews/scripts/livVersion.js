9a3df42fdaf8cb4bd942a08ab32f923e52414fcb
Kepler Siguineau <grazier.server@gmail.com>
2026-07-14 12:50:51 -0400

    fixed the liv lakay upload

--- livViews/scripts/livVersion.js
+++ livViews/scripts/livVersion.js
@@ -1,5 +1,5 @@
 /** Bump this on each Liv Lakay release push. */
-export const LIV_APP_VERSION = "0.0.0.5";
+export const LIV_APP_VERSION = "0.0.0.6";
 
 export function livVersionLabel() {
   return "v" + LIV_APP_VERSION;
